5d20164a57b568f5037dd6a6c74bdda0b4cf148a
Dirk Eddelbuettel <edd@debian.org>
2010-10-18 19:37:38 +0000

    minor tweaks

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
-".onLoad" <- function (lib, pack) {
-    library.dynam(pack, pack, lib)
+.onLoad <- function (lib, pack) {
     packageStartupMessage("RcppDE package")
     packageStartupMessage("  C++ Implementation of Differntial Evolution Optimisation")
     packageStartupMessage("  Author: Dirk Eddelbuettel")
-    packageStartupMessage("Extending:")
+    packageStartupMessage("Based on:")
     packageStartupMessage("  DEoptim package")
     packageStartupMessage("  Differential Evolution algorithm in R")
     packageStartupMessage("  Authors: David Ardia and Katharine Mullen")
