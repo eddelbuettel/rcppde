49d165249add57fb90836701c7cd9ac928aa8dad
Dirk Eddelbuettel <edd@debian.org>
2010-12-03 04:06:17 +0000

    more on the vignette for RcppDE minor onLoad edit as well

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 .onLoad <- function (lib, pack) {
-    packageStartupMessage("RcppDE package")
-    packageStartupMessage("  C++ Implementation of Differential Evolution Optimisation")
+    packageStartupMessage("Now loading:)
+    packageStartupMessage("  RcppDE: C++ Implementation of Differential Evolution Optimisation")
     packageStartupMessage("  Author: Dirk Eddelbuettel")
     packageStartupMessage("Based on:")
-    packageStartupMessage("  DEoptim package")
-    packageStartupMessage("  Differential Evolution algorithm in R")
-    packageStartupMessage("  Authors: David Ardia and Katharine Mullen")
+    packageStartupMessage("  DEoptim (version 2.0-7): Differential Evolution algorithm in R")
+    packageStartupMessage("  Authors: David Ardia, Katharine Mullen, Brian Peterson and Joshua Ulrich")
 }
